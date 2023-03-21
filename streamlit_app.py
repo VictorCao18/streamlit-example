997345b6525c243b532b62729347657f165e36d9
VictorCao18 <115897160+VictorCao18@users.noreply.github.com>
2023-03-21 14:37:43 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,13 +12,13 @@ def driving_cost(miles_per_gallon, dollars_per_gallon, miles_driven):
     return dollars 
 
 
-miles_per_gallon = st.number_input('Insert miles per gallon')
-dollars_per_gallon = st.number_input('Insert dollar cost per gallon')
+miles_per_gallon = st.number_input('Insert miles per gallon', max_value=100.00000)
+dollars_per_gallon = st.number_input('Insert dollar cost per gallon', max_value=100.00000)
 
 miles1 = driving_cost(miles_per_gallon, dollars_per_gallon, 10.0)
 miles2 = driving_cost(miles_per_gallon, dollars_per_gallon, 50.0)
 miles3 = driving_cost(miles_per_gallon, dollars_per_gallon, 400.0)
 
-st.write(miles1)
-st.write(miles2)
-st.write(miles3)
+st.write(miles1.style.format("{:.2}"))
+st.write(miles2.style.format("{:.2}"))
+st.write(miles3.style.format("{:.2}"))

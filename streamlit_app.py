b9dedda06ae8f08b79fd0c5d4e437c34d79916fa
VictorCao18 <115897160+VictorCao18@users.noreply.github.com>
2023-03-21 13:21:37 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,15 +2,13 @@ def driving_cost(miles_per_gallon, dollars_per_gallon, miles_driven):
     dollars = (dollars_per_gallon / miles_per_gallon) * miles_driven
     return dollars 
 
-if __name__ == '__main__':
-    # Type your code here.
-    miles_per_gallon = float(input())
-    dollars_per_gallon = float(input())
+miles_per_gallon = float(input())
+dollars_per_gallon = float(input())
 
-    miles1 = driving_cost(miles_per_gallon, dollars_per_gallon, 10.0)
-    miles2 = driving_cost(miles_per_gallon, dollars_per_gallon, 50.0)
-    miles3 = driving_cost(miles_per_gallon, dollars_per_gallon, 400.0)
-    
-    print(f'{miles1:.2f}')
-    print(f'{miles2:.2f}')
-    print(f'{miles3:.2f}')
+miles1 = driving_cost(miles_per_gallon, dollars_per_gallon, 10.0)
+miles2 = driving_cost(miles_per_gallon, dollars_per_gallon, 50.0)
+miles3 = driving_cost(miles_per_gallon, dollars_per_gallon, 400.0)
+
+print(f'{miles1:.2f}')
+print(f'{miles2:.2f}')
+print(f'{miles3:.2f}')

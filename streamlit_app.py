a4094576d8addc5786cb47d9fb4e4feccec39e6f
VictorCao18 <115897160+VictorCao18@users.noreply.github.com>
2023-03-21 12:57:34 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,38 +1,16 @@
-from collections import namedtuple
-import altair as alt
-import math
-import pandas as pd
-import streamlit as st
+def driving_cost(miles_per_gallon, dollars_per_gallon, miles_driven):
+    dollars = (dollars_per_gallon / miles_per_gallon) * miles_driven
+    return dollars 
 
-"""
-# Welcome to Streamlit!
+if __name__ == '__main__':
+    # Type your code here.
+    miles_per_gallon = float(input())
+    dollars_per_gallon = float(input())
 
-Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:
-
-If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
-forums](https://discuss.streamlit.io).
-
-In the meantime, below is an example of what you can do with just a few lines of code:
-"""
-
-
-with st.echo(code_location='below'):
-    total_points = st.slider("Number of points in spiral", 1, 5000, 2000)
-    num_turns = st.slider("Number of turns in spiral", 1, 100, 9)
-
-    Point = namedtuple('Point', 'x y')
-    data = []
-
-    points_per_turn = total_points / num_turns
-
-    for curr_point_num in range(total_points):
-        curr_turn, i = divmod(curr_point_num, points_per_turn)
-        angle = (curr_turn + 1) * 2 * math.pi * i / points_per_turn
-        radius = curr_point_num / total_points
-        x = radius * math.cos(angle)
-        y = radius * math.sin(angle)
-        data.append(Point(x, y))
-
-    st.altair_chart(alt.Chart(pd.DataFrame(data), height=500, width=500)
-        .mark_circle(color='#0068c9', opacity=0.5)
-        .encode(x='x:Q', y='y:Q'))
+    miles1 = driving_cost(miles_per_gallon, dollars_per_gallon, 10.0)
+    miles2 = driving_cost(miles_per_gallon, dollars_per_gallon, 50.0)
+    miles3 = driving_cost(miles_per_gallon, dollars_per_gallon, 400.0)
+    
+    print(f'{miles1:.2f}')
+    print(f'{miles2:.2f}')
+    print(f'{miles3:.2f}')
